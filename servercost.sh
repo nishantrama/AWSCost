480284f914d69f0d2c96513b583b9eea0dadc91f
EC2 Default User <ec2-user@ip-172-31-0-211.us-west-2.compute.internal>
2022-03-09 18:28:14 +0000

    check aws

--- servercost.sh
+++ servercost.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-#Query Cost of Instance
+#Query Cost of Instae
 
+if [ aws --version =~ "^aws-cli.*" ]
+then
+  echo "AWS Host Okey"
+else
+  echo "Not an AWS Host"
+  exit 0
+fi
 
 #check if OneAgent is running
 
-#If OneAgent Running
+#if [ command -v systemctl >/dev/null ]
+#then
+#    systemctl service start
+#else
+#    /etc/init.d/service start
+#fi
+
+#ccIf OneAgent Running
 
   #Send to MINT Command-lin
 
 #If OA not running
 
   #Send via Public API
+
+
+
+
