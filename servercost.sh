3b29fdfa054d121f25cc240139c18dcba513ef8c
markie-struble-duby <markie.struble@dynatrace.com>
2022-03-09 17:50:25 +0000

    template push

---
+++ servercost.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Query Cost of Instance
+
+
+#check if OneAgent is running
+
+#If OneAgent Running
+
+  #Send to MINT Command-lin
+
+#If OA not running
+
+  #Send via Public API
